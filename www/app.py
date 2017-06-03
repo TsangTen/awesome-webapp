397678ab91f5c557dfe3233ba14760c1bdf656da
Tsang <tsangten@live.com>
2017-06-03 08:58:30 +0800

    fixed html in app.py

--- www/app.py
+++ www/app.py
@@ -14,7 +14,7 @@ from aiohttp import web
 logging.basicConfig(level=logging.INFO)
 
 def index(request):
-	return web.Response(body=b'<h1>Awesome</h1>')
+	return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
 
 @asyncio.coroutine
 def init(loop):
