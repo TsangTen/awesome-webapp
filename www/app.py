102912baa7e41a5bbf70c32a5278b84f915c7e0a
Tsang <tsangten@live.com>
2017-06-05 20:02:08 +0800

    Add function init_jinja2

--- www/app.py
+++ www/app.py
@@ -17,10 +17,33 @@ from coroweb import add_routes, add_static
 
 logging.basicConfig(level=logging.INFO)
 
+def init_jinja2(app, **kw):
+	logging.info('init jinja2...')
+	options = dict(
+		autoescape = kw.get('autoescape', True),
+		block_start_string = kw.get('block_start_string', '{%'),
+		block_end_string = kw.get('block_end_string', '%}'),
+		variable_start_string = kw.get('variable_start_string', '{{'),
+		variable_end_string = kw.get('variable_end_string', '}}'),
+		auto_reload = kw.get('auto_reload', True)
+	)
+	path = kw.get('path', None)
+	if path is None:
+		path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+	logging.info('set jinja2 template path: %s' % path)
+	env = Environment(loader=FileSystemLoader(path), **options)
+	filters = kw.get('filters', None)
+	if filters is not None:
+		for name, f in filters.items():
+			env.filters[name] = f
+	app['__templating__'] = env
+
+
+
+'''
 def index(request):
 	return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
 
-'''
 @asyncio.coroutine
 def init(loop):
 	app = web.Application(loop=loop)
