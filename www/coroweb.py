54ecfde36ef02772b07a30838956c804d2c653bf
Ten <tsangten@live.com>
2017-06-06 20:53:50 +0800

    modify

--- www/coroweb.py
+++ www/coroweb.py
@@ -88,12 +88,68 @@ class RequestHandler(object):
 	def __init__(self, app, fn):
 		self._app = app
 		self._func = fn
+		self._has_request_arg = has_request_arg(fn)
+		self._has_var_kw_arg = has_var_kw_arg(fn)
+		self._has_named_kw_args = get_named_kw_args(fn)
+		self._named_kw_args = get_named_kw_args(fn)
+		self._required_kw_args = get_required_kw_args(fn)
 	
-	@asyncio.coroutine
-	def __call__(self, request):
-		kw = ..
-		r = yield from self._func(**kw)
-		return r
+	# @asyncio.coroutine
+	# def __call__(self, request):
+	# 	kw = ..
+	# 	r = yield from self._func(**kw)
+	# 	return r
+	async def __call__(self, request):
+		kw = None
+		if self._has_var_kw_arg or self._has_named_kw_args or self._required_kw_args:
+			if request.method == 'POST':
+				if not request.content_type:
+					return web.HTTPBadRequest('Missing Content Type.')
+				ct = request.content_type.lower()
+				if ct.startswith('application/json'):
+					params = await request.json()
+					if not isinstance(params, dict):
+						return web.HTTPBadRequest('JSON body must be object.')
+					kw = params
+				elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
+					params = await request.post()
+					kw = dict(**params)
+				else:
+					return web.HTTPBadRequest('Unsupported Content-Type: %s' % request.content_type)
+			if request.method == 'GET':
+				qs = request.query_string
+				if qs:
+					kw = dict()
+					for k, v in parse.parse_qs(qs, True).items():
+						kw[k] = v[0]
+		if kw is None:
+			kw = dict(**request.match_info)
+		else:
+			if not self._has_var_kw_arg and self._named_kw_args:
+				# remove all unamed kw:
+				copy = dict()
+				for name in self._named_kw_args:
+					if name in kw:
+						copy[name] = kw[name]
+				kw = copy
+			# check named arg:
+			for k, v in request.match_info.items():
+				if k in kw:
+					logging.warning('Duplicate arg name in named arg and kw args: %s' % k)
+				kw[k] = v
+		if self._has_request_arg:
+			kw['request'] = request
+		# check required kw:
+		if self._required_kw_args:
+			for name in self._required_kw_args:
+				if not name in kw:
+					return web.HTTPBadRequest('Missing argument: %s' % name)
+		logging.info('call with args: %s' % str(kw))
+		try:
+			r = await self._func(**kw)
+			return r
+		except APIError as e:
+			return dict(error=e.error, data=e.data, message=e.message)
 
 
 def add_route(app, fn):
