2ae345434fe63e46faa98db9662a6f54e11b5a56
Ten <tsangten@live.com>
2017-06-06 11:21:20 +0800

    modified coroweb.py

--- www/coroweb.py
+++ www/coroweb.py
@@ -3,9 +3,14 @@
 
 __author__ = 'Ten Tsang'
 
-
-
-
+import asyncio
+import os
+import inspect
+import logging
+import functools
+from urllib import parse
+from aiohttp import web
+from apis import APIError
 
 def get(path):
 	'''
@@ -32,6 +37,50 @@ def post(path):
 		wrapper.__route__ = path
 		return wrapper
 	return decorator
+
+def get_required_kw_args(fn):
+	args = []
+	params = inspect.signature(fn).parameters
+	for name, param in params.items():
+		if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
+			args.append(name)
+	return tuple(args)
+
+def get_named_kw_args(fn):
+	args = []
+	params = inspect.signature(fn).parameters
+	for name, param in params.items():
+		if param.kind == inspect.Parameter.KEYWORD_ONLY:
+			args.append(name)
+	return tuple(args)
+
+def has_named_kw_args(fn):
+	params = inspect.signature(fn).parameters
+	for name, param in params.items():
+		if param.kind == inspect.Parameter.KEYWORD_ONLY:
+			return True
+
+def has_var_kw_arg(fn):
+	params = inspect.signature(fn).parameters
+	for name, param in params.items():
+		if param.kind == inspect.Parameter.VAR_KEYWORD:
+			return True
+
+def has_request_arg(fn):
+	sig = inspect.signature(fn)
+	params = sig.parameters
+	found = False
+	for name, param in params.items():
+		if name == 'request':
+			found = True
+			continue
+		if found and (
+			param.kind != inspect.Parameter.VAR_POSITIONAL 
+			and param.kind != inspect.Parameter.KEYWORD_ONLY 
+			and param.kind != inspect.Parameter.VAR_KEYWORD
+		):
+			raise ValueError('request parameter must be named parameter in function: %s%s' % (fn.__name__, str(sig)))
+	return found
 
 
 class RequestHandler(object):
