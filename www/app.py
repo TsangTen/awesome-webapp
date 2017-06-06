e838d20a5d5d3e0eab744f64547776127e228dc9
Tsang <tsangten@live.com>
2017-06-06 10:00:44 +0800

    add functions: datetime_filter and response_factory

--- www/app.py
+++ www/app.py
@@ -57,7 +57,56 @@ async def data_factory(app, handler):
 		return (await handler(request))
 	return parse_data
 
+async def response_factory(app, handler):
+	async def response(request):
+		logging.info('Response handler...')
+		r = await handler(request)
+		if isinstance(r, web.StreamResponse):
+			return r
+		if isinstance(r, bytes):
+			resp = web.Response(body=r)
+			resp.content_type = 'application/octect-stream'
+			return resp
+		if isinstance(r, str):
+			if r.startswith('redirect'):
+				return web.HTTPFoundr([9:])
+			resp = web.Response(body=r.encode('utf-8'))
+			resp.content_type = 'application/json;charset=utf-8'
+			return resp
+		if isinstance(r, dict):
+			template = r.get('__template__')
+			if template is None:
+				resp = web.Response(body=json.dumps(r, ensure_ascii=False, default=lambda o:o.__dict__).encode('utf-8'))
+				resp.content_type = 'application/json;charset=utf-8'
+				return resp
+			else:
+				resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+				resp.content_type = 'text/html;charset=utf-8'
+				return resp
+		if isinstance(r, int) and r >= 100 and r < 600:
+			return web.Response(r)
+		if isinstance(r, tuple) and len(r) == 2:
+			r, m = r
+			if isinstance(t, int) and t >= 100 and t < 600:
+				return web.ReferenceError(t, str(m))
+		# default
+		resp = web.Response(body=str(r).encode('utf-8'))
+		resp.content_type = 'text/plain;charset=utf-8'
+		return resp
+	return response
 
+def datetime_filter(t):
+	delta = int(time.time() - t)
+	if delta < 60:
+		return u'1分钟前'
+	if delta < 3600:
+		return u'%s分钟前' % (delta // 60)
+	if delta < 86400:
+		return u'%s小时前' % (delta // 3600)
+	if delta < 604800:
+		return u'%s天前' % (delta // 86400)
+	dt = datetime.fromtimestamp(t)
+	return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
 '''
 def index(request):
