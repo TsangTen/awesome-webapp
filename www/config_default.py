4404ee6ecd7d993b72cce12c440e85fa6c4bcd5c
Ten <tsangten@live.com>
2017-06-06 17:37:06 +0800

    add config files

---
+++ www/config_default.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Ten Tsang'
+
+
+configs = {
+	'db': {
+		'host': '127.0.0.1',
+		'port': 3306,
+		'user': 'root',
+		'password': '1qazxsw2',
+		'database': 'awesome'
+	},
+	'session': {
+		'secret': 'Awesome'
+	}
+}
---
+++ www/config_override.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Ten Tsang'
+
+configs = {
+	'db': {
+		'host': '127.0.0.1'
+	}
+}
