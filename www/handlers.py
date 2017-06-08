739fea8be43126e73839d6c13549e479170d0c0f
Ten <tsangten@live.com>
2017-06-08 15:49:53 +0800

    add api_register_user

--- www/handlers.py
+++ www/handlers.py
@@ -37,9 +37,41 @@ async def index(request):
 		'blogs': blogs
 	}
 
-@get('/api/users')
-async def api_get_users():
-	users = await User.findAll(orderBy='created_at desc')
-	for u in users:
-		u.password = '******'
-	return dict(users=users)
+# @get('/api/users')
+# async def api_get_users():
+# 	users = await User.findAll(orderBy='created_at desc')
+# 	for u in users:
+# 		u.password = '******'
+# 	return dict(users=users)
+
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+_RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
+
+@post('/api/users')
+async def api_register_user(*, email, name, passwd):
+	if not name or not name.strip():
+		raise APIValueError('name')
+	if not email or _RE_EMAIL.match(email):
+		raise APIValueError('email')
+	if not passwd or not _RE_SHA1.match(passwd):
+		raise APIValueError('passwd')
+	users = await User.findAll('email=?', [email])
+	if len(users) > 0:
+		raise APIError('register: failed', 'email', 'Email is already in use.')
+	uid = next_id()
+	sha1_passwd = '%s:%s' % (uid, passwd)
+	user = User(
+			id=uid, 
+			name=name.strip(), 
+			email=email, 
+			passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),
+			image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest()
+		)
+	await user.save()
+	# make session cookie:
+	r = web.Response()
+	r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+	user.passwd = '******'
+	r.content_type = 'application/json'
+	r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+	return r
