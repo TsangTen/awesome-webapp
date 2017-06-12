7b78a2848b61d576a5b0d27b01fc303e957b1be7
Ten <tsangten@live.com>
2017-06-12 17:57:42 +0800

    add api_create_blogs and manage_blogs_edit.html

--- www/handlers.py
+++ www/handlers.py
@@ -167,4 +167,22 @@ async def authenticate(*, email, passwd):
 	r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
 	return r
 
-
+@post('/api/blogs')
+async def api_create_blog(request, *, name, summary, content):
+	check_admin(request)
+	if not name or not name.strip():
+		raise APIValueError('name', 'name cannot be empty.')
+	if not summary or not summary.strip():
+		raise APIValueError('summary', 'summary cannot be empty.')
+	if not content or not content.strip():
+		raise APIValueError('content', 'content cannot be empty.')
+	blog = Blog(
+			user_id=request.__user__.id, 
+			user_name=request.__user__.name, 
+			user_image=request.__user__.image,
+			name=name.strip(),
+			summary=summary.strip(),
+			content=content.strip()
+		)
+	await blog.save()
+	return blog
