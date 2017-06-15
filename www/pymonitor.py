6682e05366fd59c634669ab42ce20cd05b1cf026
Ten <tsangten@live.com>
2017-06-15 19:07:06 +0800

    add os stage judgement

--- www/pymonitor.py
+++ www/pymonitor.py
@@ -65,8 +65,10 @@ if __name__ == '__main__':
 	if not argv:
 		print('Usage: ./pymonitor your-script.py')
 		exit(0)
-	if argv[0] != 'python3':
+	if os.name == 'posix' and argv[0] != 'python3':
 		argv.insert(0, 'python3')
+	elif os.name == 'nt' and argv[0] != 'python':
+		argv.insert(0, 'python')
 	command = argv
 	path = os.path.abspath('.')
 	start_watch(path, None)
