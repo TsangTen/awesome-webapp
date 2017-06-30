f154763eb8d3b7c44e6366d5b559482b50a5d300
Ten <tsangten@live.com>
2017-06-30 10:04:35 +0800

    add notes

--- www/apis.py
+++ www/apis.py
@@ -14,11 +14,11 @@ import functools
 
 class APIError(Exception):
 	'''
-	the base APIError which contains error(required), data(optional) and message(optional).
+		the base APIError which contains error(required), data(optional) and message(optional).
 	'''
 	
 	def __init__(self, error, data='', message=''):
-		super(APIError, self).__init__(message)
+		super(APIError, self).__init__(message)  # 初始化父类
 		self.error = error
 		self.data = data
 		self.message = message
@@ -26,8 +26,8 @@ class APIError(Exception):
 
 class APIValueError(APIError):
 	'''
-	Indicate the input value has error or invalid.
-	The data specifies the error field of input form.
+		Indicate the input value has error or invalid.
+		The data specifies the error field of input form.
 	'''
 	
 	def __init__(self, field, message=''):
@@ -36,8 +36,8 @@ class APIValueError(APIError):
 
 class APIResourceNotFoundError(APIError):
 	'''
-	Indicate the resource was not found.
-	The data specifies the resource name.
+		Indicate the resource was not found.
+		The data specifies the resource name.
 	'''
 	
 	def __init__(self, field, message=''):
@@ -46,7 +46,7 @@ class APIResourceNotFoundError(APIError):
 
 class APIPermissionError(APIError):
 	'''
-	Indicate the api has no permission.
+		Indicate the api has no permission.
 	'''
 	
 	def __init__(self, message):
@@ -54,27 +54,32 @@ class APIPermissionError(APIError):
 
 class Page(object):
 	'''
-	Page object for display pages.
+		Page object for display pages.
 	'''
 	def __init__(self, item_count, page_index=1, page_size=10):
-		self.item_count = item_count
-		self.page_size = page_size
+		self.item_count = item_count  # 博客总数量
+		self.page_size = page_size  # 每页显示博客篇数
+		# 计算页数
 		self.page_count = item_count // page_size + (1 if item_count % page_size > 0 else 0)
-		if (item_count == 0) or (page_index > self.page_count):
-			self.offset = 0
-			self.limit = 0
+		if (item_count == 0) or (page_index > self.page_count):  # 如果没有博客，或页面索引大于页数
+			self.offset = 0  # 分页的开端，本页从第几篇文章开始
+			self.limit = 0  # 分页的结尾，本页最后一篇文章的索引
 			self.page_index = 1
 		else:
 			self.page_index = page_index
 			self.offset = self.page_size * (page_index - 1)
 			self.limit = self.page_size
-		self.has_next = self.page_index < self.page_count
-		self.has_previous = self.page_index > 1
+		self.has_next = self.page_index < self.page_count  # 存在下一页
+		self.has_previous = self.page_index > 1  #  存在上一页
 
-	def __str__(self):
+	def __str__(self):  # 如果要把一个类的实例变成str，就需要实现特殊方法__str__()
 		return 'item_count: %s, page_count: %s, page_index: %s, page_size: %s, offset: %s, limit: %s' % (self.item_count, self.page_count, self.page_index, self.page_size, self.offset, self.limit)
 
-	__repr__ = __str__
+	__repr__ = __str__  # 偷懒的定义__repr__的方法
+
+	# Python 定义了__str__()和__repr__()两种方法，
+	# __str__()用于显示给用户，
+	# 而__repr__()用于显示给开发人员。
 		
 
 
